7def400b8ffd37e56992143fa23f7e9ef2d4e404
kjell <kjell1906@gmail.com>
2021-04-12 19:58:57 +0200

    Added path

--- src/main/java/com/example/hero/HeroApplication.java
+++ src/main/java/com/example/hero/HeroApplication.java
@@ -2,9 +2,19 @@ package com.example.hero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class HeroApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World from String Boot on Heroku";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeroApplication.class, args);
